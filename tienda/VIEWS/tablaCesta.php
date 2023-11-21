28f56a2e7ef690fd9a7e4118b7ffd74d42a93c3e
adrirute <art0012@alu.medac.es>
2023-11-21 14:49:22 +0100

    ultimos retoques en insertar pedido

--- tienda/VIEWS/tablaCesta.php
+++ tienda/VIEWS/tablaCesta.php
@@ -30,11 +30,6 @@
         while($fila = $resultado -> fetch_assoc()){
             $idCesta = $fila["idCesta"];
         }   
-        
-        
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $insertPedido = "INSERT INTO Pedidos (usuario, precioTotal) VALUES ('$usuario', 'COMING SOON') ";
-    }
     ?>
 
 <!-- Hacemos lista de cesta de productos -->
@@ -76,9 +71,9 @@
                             echo "</tr>";
                             $suma = $fila ["cantidad"] * $fila ["precio"];
                             $total += $suma;
-                                  
-                        }   
-                        echo "<td> Precio Total: " . $total . "</td>";                      
+                                     
+                        echo "<td> Precio Total: " . $total . "</td>";   
+                    }
                         ?>
                 </tbody>
         </table>
@@ -87,6 +82,15 @@
             <input type="submit" value="Finalizar Pedido" >
         </form>
     </div>
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $sql = "INSERT INTO Pedidos (usuario, precioTotal) VALUES  ('$usuario', '$total') ";
+        $sql
+        $conexion->query($sql);
+
+    }  
+    ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>   
 </body>
 </html>
